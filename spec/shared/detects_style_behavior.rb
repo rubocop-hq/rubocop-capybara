56d4d66ae449d4673edc8417ae03715a0230eba2
Phil Pirozhkov <hello@fili.pp.ru>
2022-12-18 15:29:17 +0300

    Remove irrelevant files; move/rename relevant

--- spec/shared/detects_style_behavior.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_examples 'detects style' do |source, style, filename: 'x_spec.rb'|
-  it 'generates a todo based on the detected style' do
-    inspect_source(source, filename)
-
-    expect(cop.config_to_allow_offenses).to eq('EnforcedStyle' => style)
-  end
-end
